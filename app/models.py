fc629a9921e8facb32081aacef53a9b080e75f7f
pradepghr <pradepghr@gmail.com>
2018-11-16 19:48:13 +0545

    test cases passed

---
+++ app/models.py
@@ -0,0 +1,30 @@
+from app import db
+
+
+class BucketList(db.Model):
+    __tablename__ = 'bucketlists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp()
+    )
+
+    def __init__(self, name):
+        """initialize with name"""
+        self.name = name
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return BucketList.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def repr__(self):
+        return "<BucketList: {}>".format(self.name)
